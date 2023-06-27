646e0c24edaf84093cb76a95ba15e38234fa54f2
HikmetYigit34 <136268148+HikmetYigit34@users.noreply.github.com>
2023-06-27 18:47:18 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,76 +18,52 @@ with open(filename, mode='r') as csvFile:
         # print(row)
         rowCount += 1
         csvData.append(row)
-votesTotal=rowCount-1 # total votes are equal to rowCount-1, -1 because of title row
+votesTotal=rowCount-1                   # total votes are equal to rowCount-1, -1 because of title row
 
 # collect candidates names only, in another list
 candidates=[]
 for c in range(rowCount):
-    candidates.append(csvData[c][2]) # index [2] which is col 3, is candidate names.
+    candidates.append(csvData[c][2])    # index [2] which is col 3, is candidate names.
 
 # function for collect items, one at a time in a given list
-unique_list = [] # declare variable for a list
+unique_list = []                        # declare variable for a list
 def unique():
     for x in candidates:
         # check if exists in unique_list or not
         if x not in unique_list:
-            unique_list.append(x) # unique list now have candidate names, 3 names only and title 'Candidate'
-    # print list, this section is for testing unique list, which has 3 names in it
-    # for x in unique_list:
-        # print (x) 
-        # unique_list is, ['Candidate', 'Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane'] after checking print out.
-unique() # calling function to run, as functions are not running unless called
+            if (x!='Candidate'):        # not to include column title 'Candidate'
+                unique_list.append(x)   # unique list now have candidate names, if new names added to election_data.csv, 
+                                        # candidata to be listed in unique_list
+unique()                                # calling function to run, as functions are not running unless called
 
-# votes for each candidate, we found 3 candidates in unique_list, index[0] is fieldname
-nameForCandid1=unique_list[1]
-nameForCandid2=unique_list[2]
-nameForCandid3=unique_list[3]
+namesForCandidate=[]
+votesForCandidate=[]
+votesPercentForCandidate=[]
 
-votesForCandid1=0
-votesForCandid2=0
-votesForCandid3=0
-
-votesPercentForCandid1=0
-votesPercentForCandid2=0
-votesPercentForCandid3=0
-
-# vote counts for each candidate
-for v in range(rowCount):
-    if (nameForCandid1==csvData[v][2]): # is Charles Casper Stockham
-        votesForCandid1 += 1    
-    
-    if (nameForCandid2==csvData[v][2]): # is Diana DeGette
-        votesForCandid2 += 1
-
-    if (nameForCandid3==csvData[v][2]): # is Raymon Anthony Doane
-        votesForCandid3 += 1
-
-# find the max votes
-maxVotes = max(votesForCandid1, votesForCandid2, votesForCandid3)
-# find the candidate got max votes
-if (maxVotes==votesForCandid1):
-    winner='Charles Casper Stockham'
-elif (maxVotes==votesForCandid2):
-    winner='Diana DeGette'
-elif (maxVotes==votesForCandid3):
-    winner='Raymon Anthony Doane'
+for cand in range(len(unique_list)):                # iteration for each candidate
+    cnt=0
+    for vote in range(rowCount):
+        if(unique_list[cand]==csvData[vote][2]):
+            cnt += 1                                # vote count for each candidate
+    votesForCandidate.append(cnt)                   # list item create for each candidate number of votes
+    votesPercentForCandidate.append(cnt)            # list item create for each candidate percentage of votes
 
 # votesPercent values
-votesPercentForCandid1 = round(100*votesForCandid1/votesTotal, 3)
-votesPercentForCandid2 = round(100*votesForCandid2/votesTotal,   3)
-votesPercentForCandid3 = round(100*votesForCandid3/votesTotal,  3)
+for i in range(len(unique_list)):
+    votesPercentForCandidate[i] = round(100*votesForCandidate[i] / votesTotal, 3)     # percent calc for each candidate  
 
-# Output Results, converting numbers to string and add required signs like %, (), etc...
-votesTotal      = str(votesTotal)
-votesForCandid1 = str(votesPercentForCandid1) + '%   (' + str(votesForCandid1) + ')'
-votesForCandid2 = str(votesPercentForCandid2) + '%   (' + str(votesForCandid2) + ')'
-votesForCandid3 = str(votesPercentForCandid3) + '%   (' + str(votesForCandid3) + ')'
-winner          = str(winner)
+# find the candidate got max votes
+maxVotes = max(votesForCandidate)
+for i in range(len(unique_list)):
+    if(maxVotes==votesForCandidate[i]):
+        winner=unique_list[i]
 
+# Output Results,
 # write all output to a string, then this string to be send to: 
 # 1) print to terminal and 
 # 2) export to a text file
 
+# title
 outputStr = ""
 outputStr += "----------------------------------------------------------------------"
 outputStr += "\n         Module 3 Challenge Python PyPoll - Hikmet Yigit            "
@@ -98,21 +74,23 @@ outputStr += "\n"
 outputStr += "\n----------------------------------------------------------------------"
 outputStr += "\n                        Election Results                              "
 outputStr += "\n----------------------------------------------------------------------"
+
+# total votes
 outputStr += "\n"
-outputStr += "\n Total Votes                   :" + votesTotal
+outputStr += "\n Total Votes  : " + str(votesTotal)
 outputStr += "\n"
-outputStr += "\n"
-outputStr += "\n Charles Casper Stockham       :" + votesForCandid1
-outputStr += "\n"
-outputStr += "\n"
-outputStr += "\n Diana DeGette                 :" + votesForCandid2
-outputStr += "\n"
-outputStr += "\n"
-outputStr += "\n Raymon Anthony Doane          :" + votesForCandid3
-outputStr += "\n"
+
+# candidates, repeated for each candidate, if any candidate added to election_data.csv it will be showed up here in the results.
+# you can add "123456,Toronto,Hikmet" at the end of csv file and you will see one more candidate (Hikmet) with 1 vote, in the results
+for i in range(len(unique_list)):
+    outputStr += "\n"
+    outputStr += "\n " + str(unique_list[i]) + " : " + str(votesPercentForCandidate[i]) +"%   ("+ str(votesForCandidate[i]) +")"
+    outputStr += "\n"
+
+# winner
 outputStr += "\n----------------------------------------------------------------------"
 outputStr += "\n"
-outputStr += "\n Winner                        :" + winner
+outputStr += "\n Winner : " + str(winner)
 outputStr += "\n"
 outputStr += "\n----------------------------------------------------------------------"
 
